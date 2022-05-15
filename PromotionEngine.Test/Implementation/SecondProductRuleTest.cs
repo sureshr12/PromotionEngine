a6123b6b89fe2b99ad822b58e53eb543e7cedcd7
sureshr12 <sureshr12@gmail.com>
2022-05-15 21:55:27 +0100

    Second Product Rule added

---
+++ PromotionEngine.Test/Implementation/SecondProductRuleTest.cs
@@ -0,0 +1,50 @@
+﻿
+
+namespace PromotionEngine.Test.Implementation
+{
+    using Xunit;
+    using System.Collections.Generic;
+    using PromotionEngine.Rules;
+    using PromotionEngine.Model;
+
+    [Collection("Sequential")]
+    public class SecondProductRuleTest
+    {
+        private readonly SecondProductRule underTest;
+        private Cart cart;
+        public SecondProductRuleTest()
+        {
+            underTest = new SecondProductRule();
+        }
+
+        [Fact]
+        public void RuleShouldBeApplied_WhenValidProduct()
+        {
+            cart = new Cart()
+            {
+                Products = new List<Product>()
+                {
+                    new Product(){ Name="B", Price=30, Quantity=3}
+                }
+            };
+            var actualCart = underTest.Execute(cart);
+            Assert.Equal(75, actualCart.TotalPrice);
+            Assert.Equal(15, actualCart.DiscountPrice);
+        }
+
+        [Fact]
+        public void RuleShouldNotBeApplied_WhenInValidProduct()
+        {
+            cart = new Cart()
+            {
+                Products = new List<Product>()
+                {
+                    new Product(){ Name="C", Price=30, Quantity=3}
+                }
+            };
+            var actualCart = underTest.Execute(cart);
+            Assert.Equal(90, actualCart.TotalPrice);
+            Assert.Equal(0, actualCart.DiscountPrice);
+        }
+    }
+}
---
+++ PromotionEngine/Rules/SecondProductRule.cs
@@ -0,0 +1,25 @@
+﻿namespace PromotionEngine.Rules
+{
+    using System.Linq;
+    using PromotionEngine.Interface;
+    using PromotionEngine.Model;
+
+    /// <summary>
+    /// Apply rule 2 of B's for 45 
+    /// </summary>
+    public class SecondProductRule : IProductRule
+    {
+        private const string productName = "B";
+
+        public Cart Execute(Cart cart)
+        {
+            var productB = cart.Products.FirstOrDefault(p => p.Name.Equals(productName));
+            if (productB != null)
+            {
+                productB.Discount = productB.TotalPrice - (productB.Quantity / 2 * 45 + productB.Quantity % 2 * productB.Price);
+            }
+
+            return cart;
+        }
+    }
+}
